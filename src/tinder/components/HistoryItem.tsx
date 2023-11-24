5f2f162d702b40532c1418d41f2e45997b031aa3
pika2uuu <foo@bar.com>
2023-11-25 06:57:40 +0900

    クイックルック

--- src/tinder/components/HistoryItem.tsx
+++ src/tinder/components/HistoryItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, HStack, Text, Image, Link, Center } from "@chakra-ui/react";
+import { Box, HStack, Text, Image, Link, Center, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, Button } from "@chakra-ui/react";
 import { ViewIcon, DeleteIcon, ExternalLinkIcon } from "@chakra-ui/icons";
 import * as moment from "moment-timezone";
 import { TabData } from "../../types/tabData";
@@ -13,16 +13,13 @@ const HistoryItem: React.FC<HistoryItemProps> = ({ tabData, onDelete }) => {
   const lastseen = moment(tabData.lastseen);
   lastseen.locale("ja");
   const fromNow = lastseen.fromNow();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
       <HStack spacing='5px' _hover={{ ".delete-icon": { visibility: "visible" } }}>
         <Center w='20x' marginRight='3px' className='delete-icon' visibility='hidden'>
-          <ViewIcon
-            color='green.300'
-            cursor='pointer'
-            boxSize={4}
-          />
+          <ViewIcon color='green.300' cursor='pointer' boxSize={4} onClick={onOpen} />
         </Center>
         <Center w='20x' marginRight='3px' className='delete-icon' visibility='hidden'>
           <DeleteIcon
@@ -54,6 +51,28 @@ const HistoryItem: React.FC<HistoryItemProps> = ({ tabData, onDelete }) => {
         <Box w='150px'>
           <Text color={"gray.200"}>{fromNow}</Text>
         </Box>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Image src={tabData.screenShot} alt='Dan Abramov' />
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                閉じる
+              </Button>
+              <Button colorScheme='red' onClick={(e) => {
+                e.stopPropagation();
+                onDelete();
+              }}>
+                削除する
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </HStack>
     </>
   );
